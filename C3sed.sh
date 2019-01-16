c437a0d94171d06f32d8654cf2512b77fcfc0812
GordonWong1998 <43144890+GordonWong1998@users.noreply.github.com>
2019-01-16 01:01:32 -0500

    Update C3sed.sh

--- C3sed.sh
+++ C3sed.sh
@@ -1,3 +1,6 @@
+# This script assign the following correction function to the mz_correction_function parameter, and put the newly 
+# created/modified Snakefile into different directories.
+
 for i in none regression_delta_ppm unweighted_regression weighted_regression quadratic_regression weighted_quadratic_regression weighted_quadratic_regression_delta_ppm quadratic_regression_delta_ppm
 do
         mkdir -p C3/$i
